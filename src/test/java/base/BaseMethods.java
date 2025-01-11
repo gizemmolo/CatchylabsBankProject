9f7f6a3640b10bffa90fdf5a12470efdff9ce8a0
gizemmolo <gizemmolo@gmail.com>
2025-01-11 17:46:32 +0300

    BaseTest sinifina farklı taraycilar arasi test entegrasyonu saglandi ve Base Methods sinifina temel metotlar yazildi

--- src/test/java/base/BaseMethods.java
+++ src/test/java/base/BaseMethods.java
@@ -1,4 +1,85 @@
 package base;
 
-public class BaseMethods {
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseMethods extends BaseTests {
+
+
+    public static void wait(By by) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    public static String checkUrl() {
+        return driver.getCurrentUrl();
+    }
+
+    public static WebElement findElement(By by) {
+        wait(by);
+        return driver.findElement(by);
+    }
+
+    public static boolean isDisplayedBy(By by) {
+        try {
+            return findElement(by).isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    public static void clickElement(By by) {
+        wait(by);
+        findElement(by).click();
+    }
+
+    public static void checkAndClickElement(By by) {
+        wait(by);
+        if (isDisplayedBy(by)) {
+            findElement(by).click();
+        }
+    }
+
+    public static void sendKey(By by, String text) {
+        wait(by);
+        findElement(by).sendKeys(text);
+    }
+
+    public static void clear(By by) {
+        findElement(by).clear();
+    }
+
+    public static String getText(By by) {
+        wait(by);
+        return findElement(by).getText();
+    }
+
+    public static boolean checkText(By by, String expText) {
+        wait(by);
+        String text = getText(by);
+        if (text.contains(expText)) {
+            System.out.println("Elementin Text Degeri " + expText + " Degerini Iceriyor");
+            return true;
+        } else {
+            System.out.println("Elementin Text degeri Beklenen " + expText + " Text Degerini Icermiyor");
+            return false;
+        }
+    }
+
+
+    public void scrollToElement(By by) {
+        wait(by);
+        WebElement element = driver.findElement(by);
+        new Actions(driver).scrollToElement(element).perform();
+    }
+
+
 }
+
+
--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,4 +1,77 @@
 package base;
 
+import com.thoughtworks.gauge.AfterScenario;
+import com.thoughtworks.gauge.BeforeScenario;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class BaseTests {
+
+    protected static WebDriver driver = null;
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @BeforeScenario
+    public void setupDriver() {
+        String browser = System.getProperty("browser", "chrome").toLowerCase();
+
+        if (browser == null || browser.isEmpty()) {
+            browser = "chrome";
+        }
+
+        switch (browser) {
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--disable-notifications");
+                chromeOptions.addArguments("--disable-application-cache");
+                driver = new ChromeDriver(chromeOptions);
+                driver.manage().deleteAllCookies();
+                break;
+
+            case "firefox":
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--disable-notifications");
+                firefoxOptions.addArguments("--disable-application-cache");
+                driver = new FirefoxDriver(firefoxOptions);
+                driver.manage().deleteAllCookies();
+                break;
+
+            case "edge":
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--disable-notifications");
+                edgeOptions.addArguments("--disable-application-cache");
+                driver = new EdgeDriver(edgeOptions);
+                driver.manage().deleteAllCookies();
+                break;
+
+            default:
+                logger.info("Geçersiz tarayıcı seçildi:" + browser + "Varsayılan olarak Chrome kullanılacak.");
+                ChromeOptions defaultOptions = new ChromeOptions();
+                defaultOptions.addArguments("--disable-notifications");
+                defaultOptions.addArguments("--disable-application-cache");
+                driver = new ChromeDriver(defaultOptions);
+                driver.manage().deleteAllCookies();
+                break;
+        }
+
+        logger.info("browser acilir.");
+        driver.manage().window().maximize();
+        logger.info("Catchylabs sitesine giris yapilir. ");
+    }
+
+    @AfterScenario
+    public void tearDown() {
+        if (driver != null) {
+            driver.manage().deleteAllCookies();
+            driver.quit();
+            logger.info("browser kapatilir.");
+        }
+    }
 }
